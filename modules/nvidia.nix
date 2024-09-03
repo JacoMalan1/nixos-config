10c60d2d3ac6764b6d97b27c15cf8b532a34a3b4
Jaco Malan <jacomalan.12@gmail.com>
2024-09-03 17:20:22 +0200

    hotbox: Generation 257

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -21,7 +21,7 @@
 
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = true;
+    powerManagement.enable = false;
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
