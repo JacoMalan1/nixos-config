0e54b51e204022f57a40e561f48ffe434fe92a60
Jaco Malan <jacomalan.12@gmail.com>
2024-09-06 15:25:45 +0200

    Change home-manager version to 24.05

--- flake.nix
+++ flake.nix
@@ -5,8 +5,8 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     home-manager = { 
-      url = "github:nix-community/home-manager"; 
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      url = "github:nix-community/home-manager/release-24.05"; 
+      inputs.nixpkgs.follows = "nixpkgs-stable";
     };
     nixpkgs-mongodb-pin.url = "github:NixOS/nixpkgs/2527da1ef492c495d5391f3bcf9c1dd9f4514e32";
     strain = {
@@ -41,7 +41,7 @@
         modules = [ ./home/jacom/hotbox.nix ];
       };
       workhorse-jacom = home-manager.lib.homeManagerConfiguration {
-        pkgs = import inputs.nixpkgs-unstable { inherit system; config.allowUnfree = true; };
+        pkgs = import inputs.nixpkgs-stable { inherit system; config.allowUnfree = true; };
         modules = [ ./home/jacom/workhorse.nix ];
       };
     };
