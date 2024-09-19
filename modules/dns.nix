31bef600ec9867f976b810eafcd6fdf02335924c
Jaco Malan <jacomalan.12@gmail.com>
2024-09-19 23:19:55 +0200

    workhorse: Generation 47

--- modules/dns.nix
+++ modules/dns.nix
@@ -1,10 +1,16 @@
-{ ... }: {
-  networking.nameservers = [ "194.242.2.2#dns.mullvad.net" ];
+{ lib, ... }: {
+  networking.nameservers = lib.mkDefault [ "194.242.2.2#dns.mullvad.net" ];
+  services.resolved.fallbackDns = lib.mkDefault [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+
+  specialisation."cloudflare-dns".configuration = {
+    networking.nameservers = [ "1.1.1.1#one.one.one.one" ];
+    services.resolved.fallbackDns = [ "1.0.0.1#one.one.one.one" ];
+  };
   
   services.resolved = {
     enable = true;
     domains = [ "~." ];
-    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
     dnsovertls = "true";
   };
+  
 }
