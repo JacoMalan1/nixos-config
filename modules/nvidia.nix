926459b3ce40144c8e4e6041f9950b618263d844
Jaco Malan <jacomalan.12@gmail.com>
2024-10-28 19:19:54 +0200

    Updated NVidia driver version in nvidia module.

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -19,19 +19,25 @@
     "i2c-nvidia_gpu"
   ];
 
+  environment.systemPackages = with pkgs; [
+    vulkan-headers
+    vulkan-loader
+    vulkan-validation-layers
+  ];
+
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
     open = false;
-    nvidiaSettings = true;
+    nvidiaSettings = false;
     
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "555.58";
-      sha256_64bit = "sha256-bXvcXkg2kQZuCNKRZM5QoTaTjF4l2TtrsKUvyicj5ew=";
+      version = "560.35.03";
+      sha256_64bit = "sha256-8pMskvrdQ8WyNBvkU/xPc/CtcYXCa7ekP73oGuKfH+M=";
       sha256_aarch64 = lib.fakeSha256;
       openSha256 = lib.fakeSha256;
-      settingsSha256 = "sha256-vWnrXlBCb3K5uVkDFmJDVq51wrCoqgPF03lSjZOuU8M=";
+      settingsSha256 = "sha256-kQsvDgnxis9ANFmwIwB7HX5MkIAcpEEAHc8IBOLdXvk=";
       persistencedSha256 = lib.fakeSha256;
     };
   };
