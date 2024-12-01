de8391aadad3d173265cede641e7b119bf2b2296
Jaco Malan <jacomalan.12@gmail.com>
2024-12-01 23:07:51 +0200

    Added docker module

---
+++ modules/docker.nix
@@ -0,0 +1,17 @@
+{ inputs, system, ... }:
+let
+  pkgs = import inputs.nixpkgs-stable { inherit system; };
+in
+{
+  environment.systemPackages = with pkgs; [
+    skopeo
+  ];
+  
+  virtualisation = {
+    containers.enable = true;
+    docker.rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+  };
+}
