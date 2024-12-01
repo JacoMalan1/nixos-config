2d3e36384fabc82edfe72216f9d0c55711ed5121
Jaco Malan <jacomalan.12@gmail.com>
2024-12-01 23:07:45 +0200

    Added print module

---
+++ modules/print.nix
@@ -0,0 +1,16 @@
+{ inputs, system, ... }: 
+let
+  pkgs = import inputs.nixpkgs-stable { inherit system; config.allowUnfree = true; };
+in
+{
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [
+      cnijfilter2
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    system-config-printer
+  ];
+}
