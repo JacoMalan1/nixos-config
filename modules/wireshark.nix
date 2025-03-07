4b1ab7202d00a05eef3d085aeeb448bb75bef81a
Jaco Malan <jacomalan.12@gmail.com>
2025-03-07 07:54:14 +0200

    Add wireshark module

--- modules/wireshark.nix
+++ modules/wireshark.nix
@@ -1,4 +1,8 @@
-{ ... }: {
+{ inputs, system, ... }:
+let pkgs = import inputs.nixpkgs-stable { inherit system; };
+in {
   programs.wireshark.enable = true;
   users.users.jacom.extraGroups = [ "wireshark" ];
+
+  environment.systemPackages = with pkgs; [ wireshark-qt ];
 }
