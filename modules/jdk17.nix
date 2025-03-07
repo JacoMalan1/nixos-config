b8d53bc4723aaaf8687473d846a2a5bfff3ad962
Jaco Malan <jacomalan.12@gmail.com>
2025-03-07 07:54:08 +0200

    Add jdk17 module

--- modules/jdk17.nix
+++ modules/jdk17.nix
@@ -1,6 +1,6 @@
 { inputs, system, ... }:
 let pkgs = import inputs.nixpkgs-stable { inherit system; };
 in {
-  environment.systemPackages = with pkgs; [ jdk17 ];
-  environment.sessionVariables = { JAVA_HOME = "${pkgs.jdk17}/lib/openjdk"; };
+  environment.systemPackages = with pkgs; [ jdk21 ];
+  environment.sessionVariables = { JAVA_HOME = "${pkgs.jdk21}/lib/openjdk"; };
 }
