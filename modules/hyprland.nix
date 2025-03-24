d7cd856d56bb7034d824fefbdb76d6ea3696d50f
Jaco Malan <jacom@pentamotorgroup.co.za>
2025-03-24 08:49:21 +0200

    django: Generation 18

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -6,6 +6,7 @@ in {
   programs.hyprland = {
     enable = true;
     package = pkgs-unstable.hyprland;
+    portalPackage = pkgs-unstable.xdg-desktop-portal-hyprland;
     xwayland.enable = true;
   };
 
