2937e0404db0eb8cc26bf6065658441ee7af306c
Jaco Malan <jacomalan.12@gmail.com>
2025-04-10 19:59:24 +0200

    jacom@hotbox: Generation 346

--- home/jacom/hotbox.nix
+++ home/jacom/hotbox.nix
@@ -1,6 +1,30 @@
 { ... }: {
   imports = [ ./common.nix ./hyprland/hotbox.nix ./waybar/hotbox.nix ./nixvim ./rofi ];
 
+  xdg.desktopEntries = {
+    spotify = {
+      name = "Spotify";
+      genericName = "Music Player";
+      exec =
+        "spotify --enable-features=UseOzonePlatform --ozone-platform=x11 %U";
+      terminal = false;
+      icon = "spotify-client";
+      mimeType = [ "x-scheme-handler/spotify" ];
+      categories = [ "Audio" "Music" "Player" "AudioVideo" ];
+    };
+    dbeaver = {
+      name = "dbeaver-ce";
+      genericName = "Universal Database Manager";
+      terminal = false;
+      comment = "Universal Database Manager and SQL Client";
+      icon = "dbeaver";
+      settings = { StartupWMClass = "DBeaver"; };
+      startupNotify = true;
+      mimeType = [ "application/sql" ];
+      exec = "env GDK_BACKEND=x11 dbeaver";
+    };
+  };
+
   programs.mangohud = {
     enable = true;
     settings = { position = "top-right"; };
