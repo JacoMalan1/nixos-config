35ec09b684785ee799bc5e052d13ef88eb198f4c
Jaco Malan <jacomalan.12@gmail.com>
2025-05-29 19:11:05 +0200

    jacom@hotbox: Generation 371

--- home/jacom/hyprland/hotbox.nix
+++ home/jacom/hyprland/hotbox.nix
@@ -1,16 +1,22 @@
-{ ... }:
+{ inputs, system, ... }:
 let
   leftMonitor = "DP-3";
   rightMonitor = "HDMI-A-1";
+  pkgs = import inputs.nixpkgs-unstable { inherit system; };
 in {
   imports = [ ./common.nix ];
 
+  home.packages = with pkgs; [ hyprshot copyq ];
+
   wayland.windowManager.hyprland.settings = {
+    exec-once = [ "copyq --start-server" ];
     env = [ "ELECTRON_OZONE_PLATFORM_HINT,auto" "LIBVA_DRIVER_NAME,nvidia" ];
     bind = [
       "ALT, w, swapactiveworkspaces, ${rightMonitor} ${leftMonitor}"
       "ALT, h, focusmonitor, ${leftMonitor}"
       "ALT, l, focusmonitor, ${rightMonitor}"
+      "SUPER, s, exec, hyprshot -o ~/Pictures -m region"
+      "SUPER SHIFT, s, exec, hyprshot -o ~/Pictures -m window"
     ];
 
     windowrulev2 = [
